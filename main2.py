00b5f65e69dea07fe551327245cbc9cf195fe1c2
Jeong <jong73060@hotmail.com>
2020-12-23 21:33:29 +0900

    sync

--- main2.py
+++ main2.py
@@ -1,18 +1,3 @@
-''' Procedure
-1. data
-    a. X
-        - load PRE (assume Gaussian)
-        - load post: assume some reflow oven effect (angular and distance shift) from PRE
-    b. Y
-        - compute the Euclidean distance to use as the target_tensor 
-2. load model
-3. train model with X and Y
-4. sample a new PRE value from the optimized acquisition
-5. determine corresponding POST values 
-6. compute the Euclidean distance to use and append as the new target_tensor value
-7. visualize altogether iteratively
-'''
-
 import math, os, sys
 
 import yaml
@@ -36,35 +21,25 @@ if __name__ == '__main__':
     with open('config.yml', 'r')  as file:
         cfg = yaml.load(file, yaml.FullLoader)
     x_pre, y_pre, x_post, y_post = toydataGenerator(cfg, toy_boolean=True)
-    print('[INFO] pre distance:', np.sqrt(np.sum(x_pre.numpy()**2 + y_pre.numpy()**2)),
-          'post distance:', np.sqrt(np.sum(x_post.numpy()**2 + y_post.numpy()**2)))
+    # print('[INFO] pre distance:', np.sqrt(np.sum(x_pre.numpy()**2 + y_pre.numpy()**2)),
+    #       'post distance:', np.sqrt(np.sum(x_post.numpy()**2 + y_post.numpy()**2)))
     
     """ plot data """
     _bin = np.linspace(-120,120,60)
     _bins_x, _bins_y = np.meshgrid(_bin, _bin)
     _bins = np.concatenate([_bins_x.reshape(-1,1), _bins_y.reshape(-1,1)],1)
     viz_utils.contourplot(_bin, _bin, 'Train data', x_pre = x_pre, y_pre = y_pre, x_post = x_post, y_post = y_post, cfg = cfg)
-    # fig = plt.figure()
-    # ax1 = fig.add_subplot(121)
-    # simpleplot(x_pre, y_pre, ax1, title='PRE', legend='PRE')
-    # ax2 = fig.add_subplot(122)
-    # simpleplot(x_post, y_post, ax2, title='POST', legend='POST')
-    # fig.tight_layout()
-    # plt.show()
-    # contourplot(x_pre, y_pre, 'pre')
-    # contourplot(x_post, y_post, 'post')    
     
-    # objective function
-    # - to minimize the distance from the POST to origin toward zero
+    ''' objective function: to minimize the distance from the POST to origin toward zero
+    '''
     euclidean_dist = [bo_utils.objective(x1, x2) for x1, x2 in zip(x_post, y_post)]
     
-    # make into proper dimension for SurrogateModel (e.g. SingleTaskGP)
+    ''' make into proper dimension for SurrogateModel (e.g. SingleTaskGP) '''
     input_tensor = torch.cat([x_pre, y_pre], dim=1) # (N,2) dim
     target_tensor = torch.FloatTensor(euclidean_dist).unsqueeze(1) # (N,1) dim
-    # print(input_tensor.shape, target_tensor.shape)
     
     # initialize and fit model
-    surrogate = SurrogateModel(cfg=cfg, neural=True)
+    surrogate = SurrogateModel(cfg=cfg, neural=False)
     # surrogate.fitGP(input_tensor, target_tensor)
     # for NP model, set neural to True
     # surrogate.model.train() # unncessary for GPyTorch(BoTorch)-based model
